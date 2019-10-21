61e187237eb513c6d8fcde3c125d307d07ba55f4
Logan Smith <logsmith06@gmail.com>
2019-10-21 23:29:28 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,27 @@
 
 def key_for_min_value(name_hash)
 
+
+
+
+min = 10000
+name_hash.collect do |key, value|
+  if value < min
+    min = value
+
+  end
+end
+string = ""
+name_hash.collect do |key, value|
+  if value == min
+    string = key
+
+  end
+end
+if name_hash.size == 0
+  string = nil
+end
+string
+
+
 end
